12a87e8cabcdbf47a7199e42a4e669701daac79d
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-15 18:41:53 +0100

    Add deploy script for nftMarketplace

---
+++ deploy/01-deploy-nftMarketplace.js
@@ -0,0 +1,25 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ deployments, getNamedAccounts }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("---------------------------------")
+    const args = []
+    const nftMarketplace = await deploy("NftMarketplace", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(nftMarketplace.address, args)
+    }
+    log("---------------------------------")
+}
+
+module.exports.tags = ["all", "nftMarketplace", "main"]
